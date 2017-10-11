9498994fc764bd503138f0d0cf5d77d7021b96f3
LV_FIRST_SAN <MY_FISTNAME_SAN@example.com>
2017-10-11 16:34:01 +0800

    Initial Commit

---
+++ NewPodAVPlayer1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewPodAVPlayer1
+//
+//  Created by 吕超 on 2017/10/11.
+//  Copyright © 2017年 MAXTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewPodAVPlayer1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewPodAVPlayer1
+//
+//  Created by 吕超 on 2017/10/11.
+//  Copyright © 2017年 MAXTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
